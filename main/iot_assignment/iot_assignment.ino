61b059a447dbd74aab085a86b02d5a12f0a7d88e
David Gong <davidgonghongshen@gmail.com>
2023-04-17 17:05:09 +0800

    1

--- main/iot_assignment/iot_assignment.ino
+++ main/iot_assignment/iot_assignment.ino
@@ -1,35 +1,42 @@
+#include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include "ESP8266WiFi.h"
-#define SS_PIN 15
-#define RST_PIN 16
+#include <ESP8266WiFi.h>
+#define SS_PIN 15  //D8
+#define RST_PIN 16 //D0
 MFRC522 rfid(SS_PIN, RST_PIN); //实例化类
 MFRC522::MIFARE_Key key;
+//D8 SS
+//D5 SCK
+//D7 MOSI
+//D6 MISO
+//D0 RST
 
+
+
+Servo servo;
 
 
 // WiFi parameters to be configured
-const char* ssid = "FIRSTCITY"; // Write here your router's username
-const char* password = "fcuc1234"; // Write here your router's passward
+const char* ssid = "David"; // Write here your router's username
+const char* password = "DavidGHS123"; // Write here your router's passward
 
 // Init array that will store new NUID
 byte nuidPICC[4];
 
-int led = 0; //LED D3 
-int ir = 5; //ir sensor D1  
-const int trigPin = 2; // Trigger Pin of Ultrasonic Sensor OUTPUT   D4
-const int echoPin = 4; // Echo Pin of Ultrasonic Sensor INPUT  D2
-long duration, cm, inches; 
+const int led = 0; //LED D3 
+const int ir = 5; //ir sensor D1  
+const int sg = 2; //sg90 2
+int angle = 0;
 int i; 
 
+
+
 void setup() {  
-  pinMode(led, OUTPUT);  
-  pinMode(ir,INPUT);  
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
-
-
   Serial.begin(115200);
+
+
+
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
   Serial.println();
@@ -57,10 +64,22 @@ void setup() {
   Serial.println("WiFi connected");
   // Print the IP address
   Serial.println(WiFi.localIP());
+
+  pinMode(led, OUTPUT);  
+  pinMode(ir,INPUT);  
+  
+    // for sg90
+  servo.attach(sg);
+  servo.write(angle);
+
+
 }  
 
 void loop() {  
+
+  
   rfidFunction();
+  
   if (digitalRead(ir)==LOW){ //object is detected  
     Serial.println("IR LOW"); 
     digitalWrite(led,LOW);  
@@ -70,20 +89,19 @@ void loop() {
     digitalWrite(led,HIGH);  
   }  
 
-  digitalWrite(trigPin, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW); 
-  duration = pulseIn(echoPin, HIGH) / 58.0; 
-  cm = (int(duration * 100.0)) / 100.0;
-  Serial.print(cm); 
-  Serial.println("cm"); 
+     // scan from 0 to 180 degrees
+  for(angle = 10; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(15);                   
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 10; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(15);       
+  } 
 
-  // if(cm <= 30){
-  //   // tone(buzzer, 1000); // tone() is the main function to use with a buzzer, it takes 2 or 3 parameteres (buzzer pin, sound frequency, duration)
-  //   delay(1000);
-  // }
   delay(1000); 
 }  
 
@@ -121,17 +139,22 @@ void rfidFunction(){
 
     Serial.println(F("The NUID tag is:"));
     Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
+
+    Serial.print(hexToStr(rfid.uid.uidByte, rfid.uid.size));
+//    printHex(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
     Serial.print(F("In dec: "));
     printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
+
   }
   else 
   {
     Serial.println(F("The NUID tag is:"));
     Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.print(hexToStr(rfid.uid.uidByte, rfid.uid.size));
+    //printHex(rfid.uid.uidByte, rfid.uid.size);
+
     Serial.println();
     Serial.print(F("In dec: "));
     printDec(rfid.uid.uidByte, rfid.uid.size);
@@ -165,4 +188,16 @@ void printDec(byte *buffer, byte bufferSize) {
     Serial.print(buffer[i], DEC);
   }
 }
+
+//convert hex to str
+String hexToStr(byte *buffer, byte bufferSize) {
+  String str = "";
+  for (byte i = 0; i < bufferSize; i++) {
+    str += String(buffer[i], HEX);
+  }
+  return str;
+}
+
+
+
 
