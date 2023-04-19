22200088c573b3799a0a7957f03e729989a5fa43
David Gong <davidgonghongshen@gmail.com>
2023-04-19 08:00:30 +0800

    1

--- main/arduino/arduino.ino
+++ main/arduino/arduino.ino
@@ -3,14 +3,21 @@ const int led1 = 9;
 
 const int ir2 = 10;
 const int led2 = 11;
+
+//hc-04                                                                                                             
+const int trigPin = 6; //trigPin D3 
+const int echoPin = 5; //echoPin D1  
 // defines variables
 long duration;
-int distance,led1Val,led2Val;
+int led1Val,led2Val;
+int cm = 0;
 void setup() {
   pinMode(ir1,INPUT);  
   pinMode(led1, OUTPUT);  
   pinMode(ir2,INPUT);  
   pinMode(led2, OUTPUT);  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(115200);
 }
 void loop() {
@@ -36,8 +43,24 @@ void loop() {
     digitalWrite(led2,HIGH);  
     led2Val = 1;
   }  
+  hc04();
   char s[200];
-  sprintf(s, "{\"led1\":%d,\"led2\":%d}", led1Val,led2Val);
+  sprintf(s, "{\"led1\":%d,\"led2\":%d,\"cm\":%d}", led1Val,led2Val,cm);
   Serial.println(s);
-  delay(20000);
+  delay(1000);
+}
+
+void hc04(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  cm = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  // Serial.println(String("{\"cm\":")+String(cm)+String("}"));
 }
